5c9233948df334ac81ad8a56e535cd01b83348be
SophieMilash <sophie.milash@yandex.ru>
2021-08-28 19:16:04 +0200

    feat: add crash-test

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateUserEntranceData, login);
 app.post('/signup', validateUserBody, createUser);
 
