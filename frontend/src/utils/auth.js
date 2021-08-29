a98bbffa3479242a2112d725f9c5c280d583789d
SophieMilash <sophie.milash@yandex.ru>
2021-08-29 20:43:07 +0200

    fix: change base url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.domainname.students.nomoredomains.work';
 
 function checkResponse(response) {
   if (response.ok) {
